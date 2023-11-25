80abe83575f00ae818f7bd47c743309a0f3de1f9
nadhifrw <hernadhif.rawir@gmail.com>
2023-11-25 20:44:37 +0700

    express

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+var bodyParser = require("body-parser");
+var cors = require('cors')
+const port = 3000
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(cors());
+app.use(jsonParser);
+app.use(urlencodedParser);
+
+
+app.get('/', (req, res) => {
+  res.json('Hello World!')
+})
+
+app.post('/login', (req, res) => {
+
+  const username = 'nadhif'
+  const password = 'nadhif123'
+
+  if (req.body.username !== username) {
+    res.json({
+      status: "error, username not found",
+    });
+  }
+  //check if password is valid
+  if (req.body.password !== password) {
+    res.json({
+      status: "error, wrong password",
+    });
+  }
+
+  //return kondisi sukses
+  res.json({ status: "success" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
